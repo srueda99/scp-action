805be922ced77f02a57703a87fc725cf39b5fe85
srueda99 <ruedamarinsebastian@gmail.com>
2025-04-17 22:19:47 -0500

    New features and fixes

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,19 +3,19 @@
 # Checking if the key input is not empty
 if [[ "$INPUT_KEY" ]]; then
     # If it is not empty, it uses the key for the SCP
-    echo -e "${INPUT_KEY}" > key   # Creates a file with the key content
-    chmod 400 key                  # Set the key as Read-Only
+    echo -e "${INPUT_KEY}" > key.pem   # Creates a file with the key content
+    chmod 400 key.pem                  # Set the key as Read-Only
     echo "Trying SCP process with SSH key"
     # Runs the SCP command
-    scp -P $INPUT_PORT -o StrictHostKeyChecking=no -i key $INPUT_ORIGIN "$INPUT_USERNAME"@"$INPUT_HOST":"$INPUT_DESTINATION"
+    scp -P $INPUT_PORT -o StrictHostKeyChecking=no -i key.pem -r $INPUT_ORIGIN "$INPUT_USERNAME"@"$INPUT_HOST":"$INPUT_DESTINATION"
 else
     # If the keyis empty, it uses the password for the SCP
     echo "Trying SCP process with password"
     # Runs the SCP command
-    sshpass -p $INPUT_PASSWORD scp -P $INPUT_PORT -o StrictHostKeyChecking=no $INPUT_ORIGIN "$INPUT_USERNAME"@"$INPUT_HOST":"$INPUT_DESTINATION"
+    sshpass -p $INPUT_PASSWORD scp -P $INPUT_PORT -o StrictHostKeyChecking=no -r $INPUT_ORIGIN "$INPUT_USERNAME"@"$INPUT_HOST":"$INPUT_DESTINATION"
 fi
 time=$(date)
 echo "-----------------------------"
 echo "| Files copied successfully |"
 echo "-----------------------------"
-echo "::set-output name=time::$time"
+echo "time=$time" >> $GITHUB_ENV
